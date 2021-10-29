9ff00c8b6af163aab0f3a46e895c636b3e764c63
heli0dus <muromskiy2000@mail.ru>
2021-10-29 14:02:02 +0300

    adds functionality

---
+++ src/components/Button.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class Button extends React.Component {
+    constructor(props) {
+        super(props);
+        const operations = /[+-/*=%^]/;
+        this.state = {
+            symbol: props.symbol,
+        }
+    }
+
+    render() {
+        const type = this.state.type
+        return(
+            <button className={this.props.type} onClick={this.props.onClick}>
+                {this.props.symbol}
+            </button>
+        )
+    }
+}
+
+export default Button
